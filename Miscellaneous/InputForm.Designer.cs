c14d2a36f9237d46951a7c89f1fb8173988e5ed6
Phillip Smelt <phils@borisch.com>
2025-01-13 11:32:28 -0500

    1/13/25

---
+++ Miscellaneous/InputForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ABT.Test.TestLib.Miscellaneous {
+    partial class InputForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(31, 37);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(238, 22);
+            this.textInput.TabIndex = 0;
+            this.textInput.TextChanged += new System.EventHandler(this.TextInput_Changed);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Enabled = false;
+            this.buttonOK.Location = new System.Drawing.Point(109, 94);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.ButtonOK_Clicked);
+            // 
+            // InputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 154);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.textInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "InputForm";
+            this.Text = "InputForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button buttonOK;
+    }
+}
---
+++ Miscellaneous/InputForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace ABT.Test.TestLib.Miscellaneous {
+    public partial class InputForm : Form {
+        public InputForm() {
+            InitializeComponent();
+        }
+
+        public static String Show(String Title, Icon OptionalIcon = null) {
+            InputForm inputForm = new InputForm {
+                Text = Title,
+                Icon = (OptionalIcon is null ? SystemIcons.Information : OptionalIcon),
+            };
+            if (inputForm.ShowDialog() == DialogResult.OK) return inputForm.textInput.Text;
+            else return null;
+        }
+
+        private void TextInput_Changed(Object sender, EventArgs e) {
+            buttonOK.Enabled = textInput.TextLength != 0;
+        }
+
+        private void ButtonOK_Clicked(Object sender, EventArgs e) {
+            
+        }
+    }
+}
