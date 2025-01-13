898955e00b36f3395af1a4fe80270f3a2532c671
Phillip Smelt <phils@borisch.com>
2025-01-13 08:58:54 -0500

    1/13/25

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TestLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/Amphenol-Borisch-Technologies/TestLib")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Amphenol Borisch Technologies")]
 [assembly: AssemblyProduct("TestLib")]
